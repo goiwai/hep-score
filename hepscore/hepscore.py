7c02a0ef444fb449baaab9d0b1396a37aa091041
Chris Hollowell <hollowec@bnl.gov>
2020-04-30 16:46:31 -0400

    Backport docker user namespace fix.

--- hepscore/hepscore.py
+++ hepscore/hepscore.py
@@ -230,6 +230,15 @@ def proc_results(benchmark, rpath, verbose, conf):
 
 def check_userns():
     proc_muns = "/proc/sys/user/max_user_namespaces"
+    dockerenv = "/.dockerenv"
+
+    try:
+        cg = open(dockerenv, mode='r')
+        cg.close()
+        debug_print(self.NAME + " running inside of Docker.  Not enabling user namespaces.")
+        return False
+    except Exception:
+        debug_print(self.NAME + " not running inside Docker.")
 
     try:
         mf = open(proc_muns, mode='r')
