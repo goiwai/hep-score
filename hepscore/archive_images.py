750ebe488986815960eb47ca5abe80e891d9802a
Domenico Giordano <domenico.giordano@cern.ch>
2024-03-08 12:11:16 +0100

    improve comment

--- hepscore/archive_images.py
+++ hepscore/archive_images.py
@@ -62,10 +62,10 @@ def download_and_validate_remote_images(remote_archive_url, local_hash):
             if response.status == 200:
                 return json.loads(data)
             else:
-                print(f"Warning: Failed to download remote archive from {remote_archive_url}")
+                print(f"Warning: Failed to download remote archive from {remote_archive_url}.\nAssuming this archive is not available remotely. Continuing the archive process.")
                 return {}  # Return an empty dictionary
     except urllib.error.URLError as e:
-        print(f"Warning: Failed to download remote archive from {remote_archive_url}. Error {e}")
+        print(f"Warning: Failed to download remote archive from {remote_archive_url}. Error {e}.\nAssuming this archive is not available remotely. Continuing the archive process.")
         return {}  # Return an empty dictionary
     
 def create_output_directory(directory):
