b4a2c6da27c1765d460a2aa1e27a434ee066f733
naszczep <natalia.diana.szczepanek@cern.ch>
2026-03-27 10:13:07 +0100

    keep singularity bind and apptainer bind untouchad, like b1.5

--- hepscore/hepscore.py
+++ hepscore/hepscore.py
@@ -1032,13 +1032,7 @@ class HEPscore():
             logging.info("NOTE: Replaying prior results")
         else:
             if self.cec == 'singularity':
-                bad_envs = ['SINGULARITY_BIND', 'SINGULARITY_BINDPATH', 'APPTAINER_BIND', 'APPTAINER_BINDPATH']
-
-                for be in bad_envs:
-                    if be in os.environ:
-                        logger.warning("Unsetting " + be + " environment variable")
-                        os.environ.pop(be)
-
+                logger.info("Preserving SINGULARITY/APPTAINER bind environment variables for case-keep-bind-env.")
                 try:
                     self.unpack = self.resultsdir + '/unpack'
                     logger.debug("Creating singularity unpack directory %s", self.unpack)
